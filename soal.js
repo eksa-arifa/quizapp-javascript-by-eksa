11dee68552b623056d2b2f805e16d1aeed65e2e3
eksaarifa <81791231+eksaarifa@users.noreply.github.com>
2021-07-14 16:22:55 +0700

    Add files via upload

---
+++ soal.js
@@ -0,0 +1,37 @@
+let question = [
+  {
+    "quest" : "1.Berapa 3+3...",
+    "answer1" : "6",
+    "answer2" : "9",
+    "answer3" : "2",
+    "rights" : "6"
+  },
+  {
+    "quest" : "2.Berapa 9+10...",
+    "answer1" : "11",
+    "answer2" : "19",
+    "answer3" : "15",
+    "rights" : "19"
+  },
+  {
+    "quest" : "3.Berapa 50+50...",
+    "answer1" : "130",
+    "answer2" : "110",
+    "answer3" : "100",
+    "rights" : "100"
+  },
+  {
+    "quest" : "4.Berapa 35+35...",
+    "answer1" : "70",
+    "answer2" : "55",
+    "answer3" : "60",
+    "rights" : "70"
+  },
+  {
+    "quest" : "5.Berapa 60+70...",
+    "answer1" : "130",
+    "answer2" : "101",
+    "answer3" : "140",
+    "rights" : "130"
+  },
+  ];
