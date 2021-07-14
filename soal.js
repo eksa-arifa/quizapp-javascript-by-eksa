b3ae45e41022b70f1368d51db47637026ee40e53
eksaarifa <81791231+eksaarifa@users.noreply.github.com>
2021-07-14 17:11:53 +0700

    Add files via upload

---
+++ soal.js
@@ -0,0 +1,72 @@
+let question = [
+  {
+    "quest" : "1.Berapa 3+3...",
+    "answer1" : "6",
+    "answer2" : "9",
+    "answer3" : "2",
+    "rights" : "6"
+  },
+  {
+    "quest" : "2.Berapa 9+10...",
+    "answer1" : "11",
+    "answer2" : "19",
+    "answer3" : "15",
+    "rights" : "19"
+  },
+  {
+    "quest" : "3.Berapa 50+50...",
+    "answer1" : "130",
+    "answer2" : "110",
+    "answer3" : "100",
+    "rights" : "100"
+  },
+  {
+    "quest" : "4.Berapa 35+35...",
+    "answer1" : "70",
+    "answer2" : "55",
+    "answer3" : "60",
+    "rights" : "70"
+  },
+  {
+    "quest" : "5.Berapa 60+70...",
+    "answer1" : "130",
+    "answer2" : "101",
+    "answer3" : "140",
+    "rights" : "130"
+  },
+  {
+    "quest" : "6.Berapa 2+2×4...",
+    "answer1" : "8",
+    "answer2" : "10",
+    "answer3" : "7",
+    "rights" : "10"
+  },
+  {
+    "quest" : "7.Berapa 3×6...",
+    "answer1" : "18",
+    "answer2" : "20",
+    "answer3" : "16'",
+    "rights" : "18"
+  },
+  {
+    "quest" : "8.Berapa 6×6+3...",
+    "answer1" : "40",
+    "answer2" : "39",
+    "answer3" : "36",
+    "rights" : "39"
+  },
+  {
+    "quest" : "9.Berapa 5×5×15...",
+    "answer1" : "135",
+    "answer2" : "375",
+    "answer3" : "312",
+    "rights" : "375"
+  },
+  {
+    "quest" : "10.Berapa 10+12+3×5...",
+    "answer1" : "37",
+    "answer2" : "46",
+    "answer3" : "125",
+    "rights" : "37"
+  },
+  ];
